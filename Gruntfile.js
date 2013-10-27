9cd510847abdb4e2cc5d4d040641ab804e615ac2
Max Waterman <max.waterman@intel.com>
2013-10-27 14:13:22 +0000

    Adding grunt-release.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-release');
   grunt.loadTasks('tools/grunt-tasks');
 
   grunt.initConfig({
@@ -14,6 +15,14 @@ module.exports = function (grunt) {
     chromeInfo: grunt.file.readJSON('data/chrome-crx/manifest.json'),
 
     clean: ['build'],
+
+    release: {
+      options: {
+        npm: false,
+        npmtag: false,
+        tagName: 'v<%= version %>'
+      }
+    },
 
     tizen_configuration: {
       // location on the device to install the tizen-app.sh script to
@@ -74,7 +83,7 @@ module.exports = function (grunt) {
           { expand: true, cwd: '.', src: ['app/css/**'], dest: 'build/' },
           { expand: true, cwd: '.', src: ['app/fonts/**'], dest: 'build/' },
           { expand: true, cwd: '.', src: ['app/audio/**'], dest: 'build/' },
-          { expand: true, cwd: 'app/', src: ['README.txt'], dest: 'build/app/' },
+          { expand: true, cwd: '.', src: ['app/README.txt'], dest: 'build/' },
           { expand: true, cwd: '.', src: ['LICENSE'], dest: 'build/app/' },
           { expand: true, cwd: '.', src: ['app/_locales/**'], dest: 'build/' }
         ]
